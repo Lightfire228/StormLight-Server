16157c8822eb58aa5ecd0603a3c36f1c3d2f9e44
Lightfire228 <lightfire228@gmail.com>
2024-12-12 23:26:05 -0500

    add antiforgery (sorta)

--- src/Stormlight.Server/Program.cs
+++ src/Stormlight.Server/Program.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Antiforgery;
 using Stormlight.Models.Api;
 using Stormlight.Server;
 
@@ -7,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAntiforgery();
 
 var app = builder.Build();
 
@@ -16,11 +19,31 @@ if (app.Environment.IsDevelopment()) {
     app.UseSwaggerUI();
 }
 
+app.UseAntiforgery();
+
 // TODO:
 // app.UseHttpsRedirection();
 
-app.MapGet("/files", FilesApi.GetFiles)
-.WithName("GetFiles")
-.WithOpenApi();
+// app.MapGet("/token", (HttpContext context, IAntiforgery antiforgery) => {
+//     var token = antiforgery.GetAndStoreTokens(context);
+
+//     string json = JsonSerializer.Serialize(new {
+//         value      = token.RequestToken,
+//         name       = token.FormFieldName,
+//         cookie     = token.CookieToken,
+//     });
+
+//     Console.WriteLine($">>>>>>>>>>>> json {json}");
+
+//     return Results.Content(json);
+// })
+// .WithName("GetUpload")
+// .WithOpenApi();
+
+app.MapPost("/files/upload", FilesApi.FileUpload)
+    .WithName("UploadFile")
+    .WithOpenApi()
+    .DisableAntiforgery()    //TODO:
+;
 
 app.Run();
